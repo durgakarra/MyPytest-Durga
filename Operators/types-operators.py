f386b7bd93cfd04d29b3f68c9d55cd2e707d61f2
durgakarra <dkarra@faichi.com>
2023-07-01 21:53:18 +0530

    Add files via upload

---
+++ Operators/types-operators.py
@@ -0,0 +1,63 @@
+#unary operator
+
+n=7
+print(-n)
+flag = True
+print(not flag)
+
+c=5
+print(-5)
+
+b=10
+print(~b)
+
+a=5
+b=5
+print(a is b)
+
+list1 = [1,2,3]
+list2 = [1,2,3]
+print(list1 is list2)
+
+print(a is not b)
+print(list1 is not list2)
+
+x = 10
+print(x+1)
+
+y = 10
+print(-y)
+
+x=10
+x -=1
+print(x)
+
+x = 10
+y=12
+print(y > x)
+print(x<y)
+
+x = 10
+y = 10
+print(x== y)
+print (y==x)
+print(x>=y)
+print(y>=x)
+
+
+x = 10
+y = 20
+print(x!=y)
+print(x==y)
+
+print (x & y)
+
+
+a = 10
+b = 20
+print(a & b)
+print(a >> b)
+print(~a)
+print(a | b)
+
+
